3a9823ea8c14c5bc4e0885c7fd480f0a07e0b2ff
Aksana74 <detkovao185@gmail.com>
2023-05-30 22:05:33 +0300

    CODE58

---
+++ CODE54/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задайте двумерный массив. напишите программу, которая упорядочит
+//по убыванию элементы каждой строки двумерного массива.
+//Например
+//1472     7421
+//5923     9532
+//8424     8442
+
+
+
+
+int[,] table = new int[3, 4];
+FillArrayRandom(table);
+PrintArray(table);
+SortToLower(table);
+Console.WriteLine();
+PrintArray(table);
+// Метод  заполнения массива рандомными числами от 1 до 9
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Метод сортировки элементов в строке данного массива, по убыванию
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+// Вывод двумерного массива - метод (функция)
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
